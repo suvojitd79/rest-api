b6ee16d41873c3203beb5e840236a338be612eb0
suvojitd79@gmail.com <suvojitd79@gmail.com>
2019-06-14 18:29:30 +0530

    response part has been edited(3m->5d)

--- routes/token.js
+++ routes/token.js
@@ -2,7 +2,7 @@ const token = require("../auth/jwt");
 const routeToken = require("express").Router();
 
 routeToken.get("/", (req, res) => {
-  res.status(200).json({ token: token.getKey(), expiresIn: "3m" });
+  res.status(200).json({ token: token.getKey(), expiresIn: "5 days" });
 });
 
-module.exports=routeToken
+module.exports = routeToken;
